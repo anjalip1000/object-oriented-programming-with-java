690cff35241b3dfab71a024265cb9f97e049bbc7
anjalip1000 <97080895+anjalip1000@users.noreply.github.com>
2022-04-08 16:17:50 +0530

    Here in this code you can pass arguments by cmd

---
+++ arguments pass by cmd in java.java
@@ -0,0 +1,23 @@
+     
+ public class test
+
+
+{
+    public static void main(String[] args) 
+  
+  
+
+{
+      
+    System.out.println("argument length" + args.length);    
+   
+     
+
+ System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+   
+
+ }
+
+
+
+}
